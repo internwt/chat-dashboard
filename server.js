565599233e791758eebd22bf22bfcaa443ba51f1
abc <15uec032@lnmiit.ac.in>
2019-11-25 18:46:50 +0530

    dssssssssssss

---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require('express')
+var	app = express();
+var	server = require('http').createServer(app);
+var	io = require('socket.io').listen(server);
+var	usernames = [];
+var PORT = 3000
+server.listen(process.env.PORT ||PORT);
+console.log(`Server Running.. ...........  port${PORT}`);
+
+app.get('/', (req, res)=>{
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/main.css', (req, res) =>  res.sendFile(__dirname + "/" + "main.css")
+  );
+
+io.sockets.on('connection',(socket)=>{
+	console.log('Socket has Connected.......');
+
+	socket.on('new user', (data, callback)=>{
+		if(usernames.indexOf(data) != -1){
+			callback(false);
+		} else {
+			callback(true);
+			socket.username = data;
+			usernames.push(socket.username);
+			updateUsernames();
+		}
+	});
+
+	const updateUsernames=()=>{
+		io.sockets.emit('usernames', usernames);
+	}
+
+	socket.on('send message', (data)=>{
+		io.sockets.emit('new message', {msg: data, user:socket.username});
+	});
+
+	socket.on('disconnect', (data)=>{
+		if(!socket.username){
+			return;
+		}
+
+		usernames.splice(usernames.indexOf(socket.username), 1);
+		updateUsernames();
+	});
+});
+
